ed7bfe0ca59fb1217f75d237d3ec7693e1f05ba8
waeltoumi28 <waeltoumi28@gmail.com>
2023-05-17 17:10:03 +0200

    modifi

--- resources/views/admin/editcategory.blade.php
+++ resources/views/admin/editcategory.blade.php
@@ -1,31 +1,15 @@
-@extends('admin_layout.admin')
+@extends('admin_layout.layout')
 @section('title')
-    edit category
+    Edit category
 @endsection
 
-@section('content')
+@section('con')
 
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Category</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Category</li>
-                        </ol>
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
-        </section>
+    
+    @include('include.admin.admincontentheader')    
 
-        <!-- Main content -->
-        <section class="content">
+   
             <div class="container-fluid">
                 <div class="row">
                     <!-- left column -->
@@ -33,7 +17,7 @@
                         <!-- jquery validation -->
                         <div class="card card-primary">
                             <div class="card-header">
-                                <h3 class="card-title">Edit category</small></h3>
+                                <h3>@yield('title')</h3>
                             </div>
                             @if (count($errors) > 0)
                                 <div class="alert alert-danger">
--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -1,7 +1,7 @@
-@extends('admin_layout.admin')
+@extends('admin_layout.layout')
 
 @section('title')
-    Categories
+    Orders
 @endsection
 
 
@@ -10,29 +10,10 @@
     <link rel="stylesheet" href="backend/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
 @endsection
 
-@section('content')
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Ordes</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Ordes</li>
-                        </ol>
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
-        </section>
+@section('con')
+@include('include.admin.admincontentheader')
 
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
+    <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
